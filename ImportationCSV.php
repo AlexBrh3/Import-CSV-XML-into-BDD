19d8f133a180b73b05c62771b5d2072077e8cb94
AlexBrh3 <alexisbrohan68@gmail.com>
2018-03-08 14:20:35 +0100

    first commit

---
+++ ImportationCSV.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexis Brohan
+ * Date: 01/03/2018
+ * Time: 09:21
+ */
+
+function add_into_db ($dbname, $table, $host, $login, $password, $path, $filename){
+
+    try {
+        $pdo = new PDO("mysql:host=" . $host . ";port=3306;dbname=".$dbname, $login, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $base = $pdo->query('SELECT * FROM '.$table);
+        $base = $base->fetchAll(PDO::FETCH_ASSOC);
+
+        $lastData = count($base);
+
+
+        //Utilisation de la classe import
+
+
+        require "import.php";
+
+        $import = new import($path, $filename);
+
+        $ext = $import->getFileExtension();
+
+        if($ext == "csv"){
+            $data = $import->importCSV();
+            $champs = $import->getChampCSV($data);
+
+            for ($i = $lastData + 1; $i < count($data); $i++) {
+                $line = $import->getLineCSV($data, $i);
+                $pdo->exec('INSERT INTO ' . $table . ' (' . $champs . ') VALUES ('
+                    . $line . ')');
+            }
+            echo "Fichier CSV importé dans la base de donnée $dbname.";
+        }
+        else if ($ext == "xml") {
+            $data = $import->importXML();
+            $champs = $import->getChampXML($data);
+
+            
+            die;
+
+            foreach ($data->user as $users) {
+                $line = $import->getlineXML($data, $users);
+                $pdo->exec('INSERT INTO ' . $table . ' (' . $champs . ') VALUES ('
+                    . $line . ')');
+            }
+            echo "Fichier XML importé dans la base de donnée $dbname.";
+        }
+        else {
+            echo "Cette extension n'est pas encore prise en charge.";
+        }
+    }
+    catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    finally {
+        $pdo = null;
+    }
+}
+
+add_into_db("mock_data", "users", "127.0.0.1", "root", "", __DIR__, "MOCK_DATA.xml");
---
+++ import.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexi
+ * Date: 07/03/2018
+ * Time: 16:36
+ */
+
+class import
+{
+    private $_path;
+    private $_filename;
+
+    public function __construct($path, $filename)
+    {
+        $this->_path = $path;
+        $this->_filename = $filename;
+    }
+
+    public function getFileExtension(){
+
+        $filepath = $this->_path.DIRECTORY_SEPARATOR.$this->_filename;
+        $extension = new SplFileInfo($filepath);
+        $extension = $extension->getExtension();
+
+        if(empty($extension)){
+            die("Erreur : le fichier n'a pas d'extension");
+        }
+        return $extension;
+    }
+
+    public function importCSV (){
+
+        $filepath = implode(DIRECTORY_SEPARATOR, array(
+            $this->_path,
+            $this->_filename
+        ));
+
+        if (file_exists($filepath)) {
+            $dataCSV = array_map("str_getcsv", file($filepath));
+            return $dataCSV;
+        } else {
+            die("Fichier CSV inexistant.");
+        }
+    }
+
+    public function importXML (){
+
+        $filepath = implode(DIRECTORY_SEPARATOR, array(
+            $this->_path,
+            $this->_filename
+        ));
+
+        if (file_exists($filepath)) {
+            $dataXML = simplexml_load_file($filepath);
+            return $dataXML;
+        }
+        else {
+            die ("Fichier XML inexistant");
+        }
+    }
+
+    public function getLineCSV ($data, $i){
+            $line = "";
+            for ($j = 0; $j < count($data[0]); $j++) {
+                $line .= ', "' . $data[$i][$j] . '"';
+            }
+            $line = substr($line, 2);
+            return $line;
+    }
+
+    public function getChampCSV ($data){
+        $champs = "";
+        for ($i = 0; $i < count($data[0]); $i++) {
+            $champs .= ", " . $data[0][$i];
+        }
+        $champs = substr($champs, 2);
+        return $champs;
+    }
+
+    public function getlineXML ($data, $users){
+            $line = "";
+            foreach ($users as $value){
+                $line .= ', "'.$value.'"';
+            }
+            $line = substr($line, 2);
+            return $line;
+    }
+
+    public function getChampXML ($data) {
+        $champs = "";
+        foreach ($data->user[0] as $key=>$value){
+            $champs .= ", " . $key;
+        }
+        $champs = substr($champs, 2);
+        return $champs;
+    }
+
+}
+
+
